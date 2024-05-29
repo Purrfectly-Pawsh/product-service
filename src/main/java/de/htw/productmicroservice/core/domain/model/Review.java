a7816236181505d3eb5d37e56f2ae983e67fc13a
Kristiyan <74624512+krisg9@users.noreply.github.com>
2024-05-29 08:18:03 +0200

    - add db migration, reviews model & endpoint (#7)

---
+++ src/main/java/de/htw/productmicroservice/core/domain/model/Review.java
@@ -0,0 +1,31 @@
+package de.htw.productmicroservice.core.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    private UUID id;
+    private String author;
+    private String content;
+    private int rating;
+    private String title;
+    @DateTimeFormat(pattern = "dd/mm/yyyy HH:mm")
+    private LocalDateTime date;
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "product_id"), nullable = false)
+    @JsonIgnore
+    private Product product;
+}
