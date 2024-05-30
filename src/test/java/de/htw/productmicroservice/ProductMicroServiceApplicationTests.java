52c06248b120efd8c2e9d0f39b8f9777ef1da8c3
Kristiyan <74624512+krisg9@users.noreply.github.com>
2024-05-30 13:47:46 +0200

    Create CI Pipeline job (#8)
    * Create CI Pipeline job
    
    * PP-40 - remove context loads test

--- src/test/java/de/htw/productmicroservice/ProductMicroServiceApplicationTests.java
+++ src/test/java/de/htw/productmicroservice/ProductMicroServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package de.htw.productmicroservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductMicroServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
