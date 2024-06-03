4d9c250fcb45ad523b5b7e66f67ca0e4421be555
Kristiyan <74624512+krisg9@users.noreply.github.com>
2024-06-03 18:43:11 +0200

    PP-59 - CORS fix

--- src/main/java/de/htw/productmicroservice/port/user/ProductController.java
+++ src/main/java/de/htw/productmicroservice/port/user/ProductController.java
@@ -1,15 +1,12 @@
 package de.htw.productmicroservice.port.user;
 
-import de.htw.productmicroservice.core.domain.model.Category;
 import de.htw.productmicroservice.core.domain.model.Product;
 import de.htw.productmicroservice.core.domain.service.interfaces.IProductService;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1")
-@CrossOrigin(origins = "*")
 public class ProductController {
 
     private final IProductService productService;
