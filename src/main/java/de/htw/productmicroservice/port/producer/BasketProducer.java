980e7e0e8a112d4e4439849147ca0facc82c9ad1
Kristiyan <74624512+krisg9@users.noreply.github.com>
2024-06-07 11:13:07 +0200

    PP-34 - add mq connection config (#13)

--- src/main/java/de/htw/productmicroservice/port/producer/BasketProducer.java
+++ src/main/java/de/htw/productmicroservice/port/producer/BasketProducer.java
@@ -3,12 +3,14 @@ package de.htw.productmicroservice.port.producer;
 import de.htw.productmicroservice.config.RabbitMQConfig;
 import de.htw.productmicroservice.core.domain.model.Product;
 import de.htw.productmicroservice.core.domain.service.interfaces.IBasketProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import java.util.UUID;
 
 
 @Component
+@Slf4j
 public class BasketProducer implements IBasketProducer {
 
     private final RabbitTemplate productTemplate;
@@ -20,6 +22,7 @@ public class BasketProducer implements IBasketProducer {
     @Override
     public void triggerAddProductToBasketEvent(Product product, UUID basketId, int quantity) {
         ProductMessage message = ProductMapper.toProductMessage(product, basketId, quantity);
+        log.info(String.format("Sending product to basket: %s", message.toString()) );
         productTemplate.convertAndSend(RabbitMQConfig.PRODUCT_EXCHANGE, "product.add", message);
     }
 
