ab6a09579bbdb6f8be262344d79a42503534ca1e
Lor0l <o.lordieck@web.de>
2024-06-25 17:13:01 +0200

    combined nullPointer and IllegalArgument to BadRequestBodyAdvice

--- src/main/java/de/htw/productmicroservice/port/user/advice/BadRequestBodyAdvice.java
+++ src/main/java/de/htw/productmicroservice/port/user/advice/BadRequestBodyAdvice.java
@@ -1,17 +1,26 @@
 package de.htw.productmicroservice.port.user.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
+@Slf4j
 @RestControllerAdvice
-public class IllegalArgumentAdvice {
+public class BadRequestBodyAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     String illegalArgumentException(IllegalArgumentException e) {
+        log.atError().log(e.getMessage());
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String nullPointerHandler(NullPointerException e) {
+        log.atError().log(e.getMessage());
         return e.getMessage();
     }
 
